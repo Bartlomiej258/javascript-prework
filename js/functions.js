e0b1ee043fc9451b6e6a571d30c7a419d51637c3
Bartlomiej Lorenowicz <barteklorenowicz258@gmail.com>
2021-10-04 18:57:46 +0200

    poprawa kodu projektu

--- js/functions.js
+++ js/functions.js
@@ -7,6 +7,3 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
-function randomNum(min, max) {
-	return Math.floor(Math.random() * (max - min + 1) + min)
-}
